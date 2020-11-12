3367d2209752660675432d1a378e7ca465cff979
Gabriel Cini <gabrielcini07@gmail.com>
2020-11-12 11:39:21 +0100

    First Migration to Db

---
+++ SWD62AEP/ShoppingCart.Data/Context/ShoppingCartDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using ShoppingCart.Domain.Model;
+
+namespace ShoppingCart.Data.Context
+{
+    public class ShoppingCartDbContext: DbContext
+    {
+        public ShoppingCartDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Product>().Property(x => x.Id).HasDefaultValueSql("NEWID()");
+        }
+    }
+}
